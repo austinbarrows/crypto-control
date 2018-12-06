57fed2eef4a53d2d6efb24b6be759a8372aadbb6
Austin Barrows <austinm.barrows@gmail.com>
2018-12-06 17:36:39 -0500

    Set up partial application skeleton

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DashboardComponent, TableComponent]
+})
+export class DashboardModule { }
