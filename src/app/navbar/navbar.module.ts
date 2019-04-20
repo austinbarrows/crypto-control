9eeba007f0352cb0788a994460fcaefa03fa911a
Austin Barrows <austinm.barrows@gmail.com>
2019-04-20 02:27:50 -0400

    Update navbar

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { NavbarComponent } from './navbar.component';
 import { NavbarCloneComponent } from './navbar-clone/navbar-clone.component';
@@ -9,7 +11,9 @@ import { NavbarCloneComponent } from './navbar-clone/navbar-clone.component';
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   declarations: [NavbarComponent, NavbarCloneComponent],
   exports: [NavbarComponent, NavbarCloneComponent]
