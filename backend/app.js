4f457c21a6ba5cc37c993ef875a6f3d8e20be734
Austin Barrows <austinm.barrows@gmail.com>
2019-06-02 18:53:30 -0400

    Fix percentage validation

--- backend/app.js
+++ backend/app.js
@@ -516,7 +516,7 @@ async function percentOfChips(stats) {
   }
   let total = live + dead;
   let percentage = (live / total).toFixed(4);
-  if (typeof percentage !== "number") {
+  if (percentage === "NaN") {
     stats.STATS[1]["Chip%"] = 0;
     return stats;
   }
